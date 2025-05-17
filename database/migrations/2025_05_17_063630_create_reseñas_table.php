9c32d70c93a1d18809aa3c90028e14943b9975e2
Antonio <2122100585@soy.utj.edu.mx>
2025-05-17 01:26:02 -0600

    Initial commit

---
+++ database/migrations/2025_05_17_063630_create_reseñas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reseñas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('producto_id')->constrained('productos')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->text('comentario')->nullable();
+            $table->integer('calificacion')->unsigned()->between(1, 5);
+            $table->timestamps();
+
+            $table->unique(['producto_id', 'cliente_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reseñas');
+    }
+};
